92cb4ec470d3583d32ab24e44a4795ddf8ed9967
Haarika Roshini <haarika@Haarikas-MacBook-Air.local>
2023-09-14 13:29:48 +0530

    update on add data page

--- client/src/pages/addData.js
+++ client/src/pages/addData.js
@@ -4,11 +4,21 @@ import Characterlists from "../components/charactersLists";
 const AddData = () => {
     return(
         <div className="AddData">
-            <Characterlists />
-        <div style={{textAlign:"left"}}>
-        <h3>Insert data to  Characters collection</h3>
+        <div class="container">
+            <div class="row align-items-start">
+                <div class="col-6">
+                <Characterlists />
+                </div>
+                <div class="col-6">
+                <div style={{textAlign:"left"}}>
+        <h3>Add any character name</h3>
        <InsertCharacters />
        </div>
+                </div>
+            </div>
+        </div>
+       
+       
        </div>
     )}
 
