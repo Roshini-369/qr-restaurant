1884d7718142557a5a2518651a95ebf34e51235b
Roshini-369 <139566775+Roshini-369@users.noreply.github.com>
2023-09-14 12:04:33 +0530

    Update addData.js
    view character list added

--- client/src/pages/addData.js
+++ client/src/pages/addData.js
@@ -1,8 +1,10 @@
 import InsertCharacters from "../components/insertCharacter";
+import Characterlists from "../components/charactersLists";
 
 const AddData = () => {
     return(
         <div className="AddData">
+            <Characterlists />
         <div style={{textAlign:"left"}}>
         <h3>Insert data to  Characters collection</h3>
        <InsertCharacters />
