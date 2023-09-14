cb46f629657c2e8a854f233ecde10bc55c6effed
Haarika Roshini <haarika@Haarikas-MacBook-Air.local>
2023-09-14 13:16:44 +0530

    characterlist add cards

--- client/src/components/charactersLists.js
+++ client/src/components/charactersLists.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+
+
 const Characterlists = () => {
     const [message, setMessage] = useState();
     
@@ -7,20 +9,33 @@ const Characterlists = () => {
        fetch("/api/getCharacters")
         .then((res) => res.json())
         .then((res) => {
-          //console.log("res",res)
+          console.log("res",res)
           setMessage(res)
         })
     }, [message]);
     return(
       <>
-      <h3>Message from Characters : {message && message.message}</h3>
+     <div class="card" style={{ width: '250px' }}>
+      <div class="card-header">
+      Message from Characters : {message && message.message}
+  </div>
+      <div class="card-body" style={{width:'250px'}}>
       {message && message.data.map((i) =>{
         return (
-        <ul>
-        <li key={i._id}>{i.name}</li>
-        </ul>
+       
+        
+       
+
+  <ul class="list-group list-group-flush">
+   
+        <li class="list-group-item" style={{ color: '#6d3879', borderBottom:'1px solid red' }} key={i._id}>{i.name}</li>
+       
+  </ul>
+        
         )
       })}
+      </div>
+  </div>
       </>
     )
   }
